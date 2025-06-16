68a20938a02e962ad5d5f2159a32e2f4d369949a
unknown <dhdbbdhd161@gmail.com>
2025-06-16 13:19:57 +0800

    pseudocode and flow of the program

---
+++ main.py
@@ -0,0 +1,7 @@
+#start the program
+#load user data from file
+#asks the user for their username, if does not exit create a new one
+
+#main program, repeat untill the user quits
+#asks the user to prompt a command, each command with different features
+#if the command is quit, the program ends
