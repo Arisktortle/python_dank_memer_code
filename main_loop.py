f0ced6b63a7ad28253a67ffc819032f9a117d05d
unknown <dhdbbdhd161@gmail.com>
2025-06-17 23:27:53 +0800

    added main loop module.

---
+++ main_loop.py
@@ -0,0 +1,8 @@
+class MainLoop:
+    def __init__(self, data_file="data.json"):
+        self.user  = None
+        self.data_file = data_file
+        self.store_items
+        
+    def start(self):
+        print("Welcome to Dank Memer Python Edition!")
