ce48a013f88894268f7bb536ef719e5e2f28b762
unknown <dhdbbdhd161@gmail.com>
2025-06-18 18:02:11 +0800

    prompt the user to input a command to run, with error handling

--- main_loop.py
+++ main_loop.py
@@ -2,7 +2,17 @@ class MainLoop:
     def __init__(self, data_file="data.json"):
         self.user  = None
         self.data_file = data_file
+        self.command = None
         self.store_items
         
     def start(self):
         print("Welcome to Dank Memer Python Edition!")
+        
+        while True:
+            try:
+                self.command = str(input("Type \"help\" for list of commands.\n Enter a command: "))
+                continue
+            
+            except ValueError:
+                print("Invalid input, try again.")  
+            
