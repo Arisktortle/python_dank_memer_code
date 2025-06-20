5080ad764f4587356743f6adb9817fe428a1f46b
unknown <dhdbbdhd161@gmail.com>
2025-06-20 17:14:25 +0800

    re-named the show command function, added commas for each command.

--- command_list.py
+++ command_list.py
@@ -1,19 +1,17 @@
 class PrintCommands():
     
-        def command_list(self):
+        def show_commands(self):
             print("\n Commands:")
-            
-        commands = [
-            ("help", "Shows the command list")
-            ("balance", "Show wallet and bank balance")
-            ("shop", "Show items available in the shop")
-            ("buy ITEMNAME", "Buy a specific item in the shop")
-            ("inventory", "Show your inventory")
-            ("bank deposit AMOUNT", "Deposit amount of money")
-            ("bank withdraw AMOUNT", "Withdraw amount of money")
-            ("blackjack", "Play blackjack game")
-        ]
-        
-        for cmd, description in commands:
-            print(f"   {cmd.ljust(16)} ---> {description}") # type: ignore
-        print()
+            commands = [
+                ("help", "Shows the command list"),
+                ("balance", "Show wallet and bank balance"),
+                ("shop", "Show items available in the shop"),
+                ("buy ITEMNAME", "Buy a specific item in the shop"),
+                ("inventory", "Show your inventory"),
+                ("bank deposit AMOUNT", "Deposit amount of money"),
+                ("bank withdraw AMOUNT", "Withdraw amount of money"),
+                ("blackjack", "Play blackjack game"),
+            ]
+            for cmd, description in commands:
+                print(f"   {cmd.ljust(16)} - {description}") # type: ignore
+            print()
