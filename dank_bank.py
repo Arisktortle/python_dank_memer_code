6eb8a930d4287efffffced15dca56732292fee9e
unknown <dhdbbdhd161@gmail.com>
2025-06-20 16:27:05 +0800

    revised the bank system, added wallet and bank function

--- dank_bank.py
+++ dank_bank.py
@@ -1,5 +1,18 @@
 class Bank:
-    def __init__(self):
-        self.balance = 0
-        self.user_choice = None
+    def __init__(self, data=None):
+        if data:
+            self.wallet = data.get("wallet", 1000)
+            self.bank= data.get("bank", 0)
+            self.inventory = data.get("inventory", [])
+        else:
+            self.wallet = 1000
+            self.bank = 0 
+            self.inventory = []
+            
+    def to_dict(self):
+        return {
+            "wallet": self.wallet,
+            "bank": self.bank,
+            "inventory": self.inventory
+        }
         
