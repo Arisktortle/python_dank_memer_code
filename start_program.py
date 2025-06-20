f45cc3bbf8bd40c21812653fb162688f2c50ade1
unknown <dhdbbdhd161@gmail.com>
2025-06-20 17:15:26 +0800

    added the print commandlist to the start of the program

--- start_program.py
+++ start_program.py
@@ -1,12 +1,20 @@
+from command_list import PrintCommands
+
 class StartProgram():
     
     def start(self):
-        print("Welcome to Dank Memer Python Edition!")
-        
+        print("Welcome to Dank Memer Python Edition!\nType \"help\" for list of commands. \n")
+
         while True:
             try:
-                self.command = str(input("Type \"help\" for list of commands.\n Enter a command: "))
-                continue
+                self.command = str(input("Enter a command: "))
+                
+                if self.command == "help":
+                    PrintCommands().show_commands()
+            
             
             except ValueError:
                 print("Invalid input, try again.")  
+                
+start = StartProgram()
+start.start()
