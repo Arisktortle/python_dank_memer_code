dec89ef0e45827d2377f7c0a8af07283383d6d88
unknown <dhdbbdhd161@gmail.com>
2025-06-24 14:36:33 +0800

    added two functions, one that totals the cards appended inside the list and the other that calculates and prints the sum of cards inside the list

--- blackjack.py
+++ blackjack.py
@@ -7,3 +7,9 @@ class BlackjackGame:
     def draw_card(self):
         card = random.randint(2,11)
         self.hand.append(card)
+        
+    def total_cards(self):
+        return sum(self.hand)
+    
+    def show(self):
+        print(f"Total cards: {self.total_cards()}")
